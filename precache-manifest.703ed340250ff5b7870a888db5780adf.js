587d271b8188b0616f1c19088baf15af5bba78b7
Ruslan Lukianenko <ruslan201010@gmail.com>
2020-09-24 21:08:49 +0300

    Updates

---
+++ precache-manifest.703ed340250ff5b7870a888db5780adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa665e0b7023b5abfba016e13f9c4ab5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb7bacd3a92a74111223",
+    "url": "/monsters-rolodex/static/css/main.4f6fd437.chunk.css"
+  },
+  {
+    "revision": "e9b4ac5ea1cffd7d72da",
+    "url": "/monsters-rolodex/static/js/2.aa35d608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.aa35d608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7bacd3a92a74111223",
+    "url": "/monsters-rolodex/static/js/main.55d6c116.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
